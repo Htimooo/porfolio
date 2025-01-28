f82b1cac2832b367ac980717aaa7f8871fb5a223
Hugo Andrés Timote <76624227+Htimooo@users.noreply.github.com>
2025-01-28 17:49:54 -0300

    top scroll

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import ProjectDisplay from './pages/ProjectDisplay';
 import Experience from './pages/Experience';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from "./components/ScrollToTop";
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <ScrollToTop /> 
         <Navbar/>
         <Routes>
           <Route path= '/' element={<Home/>}/>
